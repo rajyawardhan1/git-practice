4734099e2066b67d35f08595d1a53898a4ed95be
rajyawardhan1 <rajyawardhan.berlin@gmail.com>
2020-12-01 10:54:52 +0530

    corrected message

--- src/main/java/com/rajyawardhan/sp/gitpractice/GitPracticeApplication.java
+++ src/main/java/com/rajyawardhan/sp/gitpractice/GitPracticeApplication.java
@@ -8,7 +8,7 @@ public class GitPracticeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitPracticeApplication.class, args);
-		System.out.println("Application Started");
+		System.out.println("Application has Started");
 	}
 
 }
