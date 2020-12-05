36090c920aac4ed8425d4f7f47a7b57f50253f16
rajyawardhan1 <rajyawardhan.berlin@gmail.com>
2020-12-06 03:47:18 +0530

    added log

--- src/main/java/com/rajyawardhan/sp/gitpractice/GitPracticeApplication.java
+++ src/main/java/com/rajyawardhan/sp/gitpractice/GitPracticeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitPracticeApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Application will Start");
 		SpringApplication.run(GitPracticeApplication.class, args);
 		System.out.println("Application has Started");
 	}
